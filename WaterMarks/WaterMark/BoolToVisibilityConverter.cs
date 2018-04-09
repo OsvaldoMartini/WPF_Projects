2a02d9aa6332b219ee9fc675af5c24a434b1dee4
Osvaldo.Martini <osvaldo.martini@gmail.com>
2018-04-09 16:46:17 +0100

    Water Mark

---
+++ WaterMarks/WaterMark/BoolToVisibilityConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WaterMark
+{
+    public class BoolToVisibilityConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            bool hasText = !(bool) values[0];
+            bool hasFocus = (bool)values[1];
+
+            if (hasText || hasFocus)
+                return Visibility.Collapsed;
+
+            return Visibility.Visible;
+
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WaterMarks/WaterMark/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace WaterMark
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_CLick(object sender, RoutedEventArgs e)
+        {
+            BindingExpression bindingExpression = TxtInput3.GetBindingExpression(TextBox.TextProperty);
+            bindingExpression.UpdateSource();
+        }
+    }
+}
