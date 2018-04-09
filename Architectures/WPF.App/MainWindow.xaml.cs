d08fe9adca8097021920cfb1937517ebcf9b2959
Osvaldo.Martini <osvaldo.martini@gmail.com>
2018-04-09 16:58:15 +0100

    Add WPF.App project

---
+++ Architectures/WPF.App/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using Modules_ViewModel;
+
+namespace WPF.App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            UserViewModel user = new UserViewModel();
+            user.UserName = "User";
+            user.IsModified = false;
+            DataContext = user;
+        }
+    }
+}
