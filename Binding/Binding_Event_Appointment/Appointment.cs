5223dc65c405b2bccadeee44dec81cd9ddddfcbb
Osvaldo.Martini <osvaldo.martini@gmail.com>
2018-04-09 14:41:49 +0100

    Add Projects

---
+++ Binding/Binding_Event_Appointment/Appointment.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Binding.Event.Appointment
+{
+    public class Appointment
+    {
+        string _eventDescription = String.Empty;
+        DateTime _eventDate = DateTime.MinValue;
+
+        public String EventDescription
+        {
+            get { return _eventDescription; }
+            set 
+            { 
+                _eventDescription = value; 
+
+            }
+        }
+
+        public DateTime EventDate
+        {
+            get { return _eventDate; }
+            set 
+            {
+                if ((value.DayOfWeek == DayOfWeek.Sunday) || (value.DayOfWeek == DayOfWeek.Saturday))
+                {
+                    throw new ArgumentException("The date must be on a weekday");
+                }
+                _eventDate = value; 
+            }
+        }
+
+    }
+}
---
+++ Binding/Binding_Event_Appointment/Window1.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows;
+
+namespace Binding.Event.Appointment
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Appointment apt = new Appointment();
+            apt.EventDescription = "Joel's Birthday";
+            apt.EventDate = new DateTime(DateTime.Now.Year, 3, 28);
+            this.DataContext = apt;
+        }
+    }
+}
