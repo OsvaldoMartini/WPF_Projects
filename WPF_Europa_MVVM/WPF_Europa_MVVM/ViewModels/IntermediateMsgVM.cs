0f90bf7e8b2f7172237eb2bcc8173c6d2748804e
Osvaldo.Martini <osvaldo.martini@gmail.com>
2018-04-24 10:52:27 +0100

    User Control YesOr No

---
+++ WPF_Europa_MVVM/WPF_Europa_MVVM/ViewModels/IntermediateMsgVM.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPF_Europa_MVVM.Foundation;
+using WPF_Europa_MVVM.Views;
+
+namespace WPF_Europa_MVVM.ViewModels
+{
+    public class IntermediateMsgVM : ViewModelBase
+    {
+        private int _messageId;
+        private string _messageInternal;
+        private string _messageScreenTransfer;
+        
+        public IntermediateMsgVM()
+        {
+
+            _messageId = 0;
+            _messageInternal =
+            _messageScreenTransfer =
+                    null;
+            //Memory Leak
+        }
+
+        public int MessageId
+        {
+            get { return _messageId; }
+            set
+            {
+                _messageId = value;
+            }
+        }
+
+        public string MessageInternal
+        {
+            get { return _messageInternal; }
+            set
+            {
+                _messageInternal = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("MessageInternal"));
+            }
+        }
+
+        public string MessageScreenTransfer
+        {
+            get { return _messageScreenTransfer; }
+            set
+            {
+                _messageScreenTransfer = value;
+                OnPropertyChanged(new PropertyChangedEventArgs("MessageScreenTransfer"));
+            }
+        }
+
+
+        private bool flag;
+        public bool Flag
+        {
+            get { return flag; }
+            set
+            {
+                if (value != flag)
+                {
+                    flag = value;
+                    OnPropertyChanged(new PropertyChangedEventArgs("Flag"));
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder strB = new StringBuilder();
+            return string.Format("{0} {1} ({2}) {3}", MessageInternal, MessageScreenTransfer, MessageId, Flag);
+        }
+
+    }
+}
---
+++ WPF_Europa_MVVM/WPF_Europa_MVVM/Views/YesOrNoMessage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using WPF_Europa_MVVM.Foundation;
+using WPF_Europa_MVVM.ViewModels;
+
+namespace WPF_Europa_MVVM.Views
+{
+    /// <summary>
+    /// Interaction logic for YesOrNoMessage.xaml
+    /// </summary>
+    public partial class YesOrNoMessage : UserControl
+    {
+        //90% of MVVM.
+        public IntermediateMsgVM MessagesModel { get; set; }
+
+        public YesOrNoMessage(IntermediateMsgVM model)
+        {
+            InitializeComponent();
+
+            //get 90% of MVVM.
+            MessagesModel = model;
+            this.GridYesOrNo.DataContext = MessagesModel;
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs args)
+        {
+            GlobalServices.ModalService.GoBackward(true);
+        }
+        private void CancelButton_Click(object sender, RoutedEventArgs args)
+        {
+            GlobalServices.ModalService.GoBackward(false);
+        }
+
+    }
+}
