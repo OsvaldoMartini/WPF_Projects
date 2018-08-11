65a9049913a07a39516edb4c7a4fb602dcd37253
Osvaldo.Martini <osvaldo.martini@gmail.com>
2018-08-11 12:25:43 +0100

    DEleted

--- Granular-master/Granular.Common.Web/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Granular.Common.Web")]
-[assembly: AssemblyDescription("")]
-
-[assembly: Bridge.Reflectable(Bridge.MemberAccessibility.None)]
