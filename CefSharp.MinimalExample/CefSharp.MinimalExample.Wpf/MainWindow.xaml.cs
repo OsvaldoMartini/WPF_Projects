28e64b5d8e6124a6b67a289aab6d82ad02824e22
Osvaldo Martini <OsvaldoMartini@users.noreply.github.com>
2018-08-15 11:41:56 +0100

    CefSharp.MinimalExample

---
+++ CefSharp.MinimalExample/CefSharp.MinimalExample.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace CefSharp.MinimalExample.Wpf
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void ExecuteJavaScriptBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //Check if the browser can execute JavaScript and the ScriptTextBox is filled
+            if (Browser.CanExecuteJavascriptInMainFrame && !string.IsNullOrWhiteSpace("Osvaldo Martini"))
+            {
+                //Evaluate javascript and remember the evaluation result
+                JavascriptResponse response = await Browser.EvaluateScriptAsync("Osvaldo Martini");
+
+                if (response.Result != null)
+                {
+                    //Display the evaluation result if it is not empty
+                    MessageBox.Show(response.Result.ToString(), "JavaScript Result");
+                }
+            }
+        }
+
+    }
+}
